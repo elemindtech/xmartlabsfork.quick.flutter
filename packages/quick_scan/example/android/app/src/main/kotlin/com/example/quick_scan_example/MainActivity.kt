ad0fa3346ae0220e5b191dedc2ecc83fde9dc474
Felipe <f7.deleon@gmail.com>
2024-07-19 12:17:26 -0300

    chore: remove unused deps

--- packages/quick_scan/example/android/app/src/main/kotlin/com/example/quick_scan_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.quick_scan_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- packages/quick_scan/ios/Classes/QuickScanPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface QuickScanPlugin : NSObject<FlutterPlugin>
-@end
